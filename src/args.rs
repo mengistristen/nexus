0cfc51622c8da279e76e1cf454d14a988bb0151a
Tristen Mengis <tristen.mengis@gmail.com>
2023-09-19 20:21:46 -0700

    Add support for parsing the create subcommand

---
+++ src/args.rs
@@ -0,0 +1,22 @@
+//! A module for parsing the command line arguments for nexus.
+pub use clap::Parser;
+use clap::{command, Subcommand};
+
+/// Command line arguments for nexus.
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Args {
+    /// The subcommand to run.
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Create a new note.
+    #[command(arg_required_else_help = true)]
+    Create {
+        /// The name of the file to create.
+        file_name: String,
+    },
+}
